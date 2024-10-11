41a65cb8730153b5b76f4bbeb7cc4833909441d7
Andres Montenegro <1997jamm@gmail.com>
2024-10-10 20:25:47 -0500

    se crea el proyecto selectivasProject y CasoDeEstudio

---
+++ selectivasProject/Main.py
@@ -0,0 +1,46 @@
+a = 33
+b = 200
+#Estructura simple
+"""if b > a:
+    print(b," es mayor que ",a)
+
+#Estructura doble
+
+if a > b:
+    print(a," es mayor que ",b)
+else:
+    print(a," no es mayor que ",b)
+
+# Estructura multiple
+
+if a > b:
+    print(a," es mayor que ",b)
+elif a < b:
+    print(a," es menor que ",b)
+else:
+    print(a," es igual que ", b)
+
+x = 28
+
+#Condiciones enlasadas
+
+if x > 10:
+    print("Por encima de Diez")
+    if x > 20 :
+        print("¡Y tambien por encima de 20!")
+    else:
+        print("Pero no por encima de 20.")
+
+#Parametros END Y SEP
+
+print("Estudiar los sabados", end= ' ')
+print("es genial")
+"""
+#Parametros END YSEP
+
+print("Daniela","Luis","Carlos","Camila")#Agrega un espacio por defecto
+print("Daniela","Luis","Carlos","Camila",sep="")#Quita espacio
+print("Daniela","Luis","Carlos","Camila",sep=",")#Agrega una coma
+
+print("Daniela", "Luis","Carlos","Camila", sep="_",end="_curso_Python")#implementacion end y sep
+
